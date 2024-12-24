d1e524f16d3aff87909536cae7ec8142e2aa629d
doufuchuan <3209189220@qq.com>
2024-12-24 16:19:32 +0800

    exercises

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -1,5 +1,5 @@
 ﻿#include "../exercise.h"
-
+#include<cstring>
 // READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
 
 template<class T>
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (int i = 0; i < 4; i++) {
+            shape[i] = shape_[i];
+            size *= shape_[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +32,25 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        for (int i = 0; i < shape[0]; ++i) {
+            int i_idx = others.shape[0] == 1 ? 0 : i;
+            for (int j = 0; j < shape[1]; ++j) {
+                int j_idx = others.shape[1] == 1 ? 0 : j;
+                for (int k = 0; k < shape[2]; ++k) {
+                    int k_idx = others.shape[2] == 1 ? 0 : k;
+                    for (int l = 0; l < shape[3]; ++l) {
+                        int l_idx = others.shape[3] == 1 ? 0 : l;
+                        data[i * shape[1] * shape[2] * shape[3] +
+                            j * shape[2] * shape[3] +
+                            k * shape[3] +
+                            l] += others.data[i_idx * others.shape[1] * others.shape[2] * others.shape[3] +
+                                    j_idx * others.shape[2] * others.shape[3] +
+                                    k_idx * others.shape[3] +
+                                    l_idx];
+                    }
+                }
+            }
+        }
         return *this;
     }
 };
